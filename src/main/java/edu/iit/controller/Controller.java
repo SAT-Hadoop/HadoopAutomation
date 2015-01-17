e6eee3697c05eeadc7f488da3d5ca9157f5ba3be
saipramod <supadyay@hawk.iit.edu>
2015-01-17 12:40:02 -0600

    push to git

--- src/main/java/edu/iit/controller/Controller.java
+++ src/main/java/edu/iit/controller/Controller.java
@@ -39,6 +39,7 @@ public class Controller extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
