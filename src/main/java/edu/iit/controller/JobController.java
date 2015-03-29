c349ad735fa73a49c06661abcd89dc2a04155478
saipramod <supadyay@hawk.iit.edu>
2015-03-29 15:00:55 -0500

    checking the jsp

--- src/main/java/edu/iit/controller/JobController.java
+++ src/main/java/edu/iit/controller/JobController.java
@@ -90,8 +90,8 @@ public class JobController extends HttpServlet {
                 System.out.println("The principal is "+request.getUserPrincipal());
                 System.out.println("awesome sai" + walrus.getObjects("sat-hadoop").toString());
                 session.setAttribute("datasets", walrus.getObjects("sat-hadoop"));
-                request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
-                ///response.sendRedirect(request.getContextPath() + "/app/index.jsp");
+                //request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+                response.sendRedirect(request.getContextPath() + "/WEB_INF/index.jsp");
                 break;
 
             case "/app/downloadfile":
